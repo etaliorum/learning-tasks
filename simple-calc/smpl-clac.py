750c890af52aae9297723dd7da27ff0795e85970
etaliorum <evgen.fw@gmail.com>
2020-01-21 16:32:01 +0200

    added code

---
+++ simple-calc/smpl-clac.py
@@ -0,0 +1,27 @@
+a = float(input())
+b = float(input())
+operand = input()
+
+if (operand == '+'):
+    print(a + b)
+if (operand == '-'):
+    print(a - b)
+if (operand == '/'):
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a / b)
+if (operand == '*'):
+    print(a * b)
+if (operand == 'mod'):
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a % b)
+if operand == 'pow':
+    print(a ** b)
+if operand == 'div':
+    if b == 0:
+        print('Деление на 0!')
+    else:
+        print(a // b)
