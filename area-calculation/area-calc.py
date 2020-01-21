178077c5f52151983601121f57ea670bdd02743a
etaliorum <evgen.fw@gmail.com>
2020-01-21 18:14:24 +0200

    added triangle code

---
+++ area-calculation/area-calc.py
@@ -0,0 +1,9 @@
+source = input()
+
+if source == 'треугольник':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    p = (a + b + c) / 2
+    s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    print(s)
