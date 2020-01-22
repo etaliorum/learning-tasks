47b9475ed59e936fc787bd77e11ce74ff44b4e53
etaliorum <evgen.fw@gmail.com>
2020-01-22 13:57:11 +0200

    find max and min, and and other

--- maximal-numbers/max-num.py
+++ maximal-numbers/max-num.py
@@ -1,3 +1,4 @@
+"""
 a = int(input())
 b = int(input())
 c = int(input())
@@ -14,3 +15,23 @@ if max >= c:
 else:
     max, c = c, max
     print(max, c, min)
+"""
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+max = max(a, b)
+min = min(a, b)
+
+if max <= c:
+    max, c = c, max
+    print(1)
+    print(max, min, c)
+elif min >= c:
+    print(2)
+    min, c = c, min
+    print(max, min, c)
+else:
+    print(3)
+    print(max, min, c)
