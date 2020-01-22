35dbd64a5ed26b484fabdc99fe9647fae24db83a
etaliorum <evgen.fw@gmail.com>
2020-01-22 13:40:16 +0200

    find max and min, and sorted

--- maximal-numbers/max-num.py
+++ maximal-numbers/max-num.py
@@ -1,8 +1,16 @@
 a = int(input())
 b = int(input())
 c = int(input())
-min = a
-max = a
 
-#поиск наибольшего числа из трех и вывести их в порядке убывания, используя только if,
-#elif, else и не используя списки, циклы, дополнительний функции такие как max() min()
+max = max(a, b)
+min = min(a, b)
+
+if max >= c:
+    if c >= min:
+        print(max, c, min)
+    else:
+        min, c = c, min
+        print(max, c, min)
+else:
+    max, c = c, max
+    print(max, c, min)
