362d70d090160daac0a75d511fdf0a193ab2e72b
etaliorum <evgen.fw@gmail.com>
2020-01-22 11:05:59 +0200

    found the word ending is correct

--- count-word/word-count.py
+++ count-word/word-count.py
@@ -1,3 +1,14 @@
 n = int(input())
+n_count = n
 
-# логика програмы как будем ваыводить правильное окончание
+if n_count >= 100:
+    n_count = n_count % 100
+if n_count > 20:
+    n_count = n_count % 10
+
+if n_count == 0 or 5 <= n_count <= 20:
+    print(n, "программистов")
+if n_count == 1:
+    print(n, "программист")
+if 2 <= n_count <= 4:
+    print(n, "программиста")
