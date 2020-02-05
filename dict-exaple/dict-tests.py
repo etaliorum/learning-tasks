7516bea902ef79f607827f632e565a8987f9f850
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2020-02-05 17:11:08 +0200

    added field problem

--- dict-exaple/dict-tests.py
+++ dict-exaple/dict-tests.py
@@ -1,3 +1,10 @@
+# Напишите функцию update_dictionary(d, key, value), которая принимает на вход словарь dd и два числа: keykey и
+# valuevalue.
+#
+# Если ключ keykey есть в словаре dd, то добавьте значение valuevalue в список, который хранится по этому ключу. Если
+# ключа keykey нет в словаре, то нужно добавить значение в список по ключу 2 * key2∗key. Если и ключа 2 * key2∗key
+# нет, то нужно добавить ключ 2 * key2∗key в словарь и сопоставить ему список из переданного элемента [value][value].
+
 def update_dictionary(d, key, value):
     if key in d:
         d[key].append(value)
